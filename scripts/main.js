174d0023623ba587b7979a2cd787cb73c65ae055
munetomo <fishmans0120@gmail.com>
2020-09-26 12:58:07 +0900

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,22 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello, world!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute('src', 'images/firefox2.png');
+  } else {
+    myImage.setAttribute('src', 'images/firefox-icon.png');
+  }
+}
+
+// const myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello, world!';
+
+// function multiply(num1, num2) {
+//   let result = num1 * num2;
+//   return result;
+// }
+
+// document.querySelector('html').onclick = function() {
+//   alert('痛っ！つつくのはやめて！');
+// }
