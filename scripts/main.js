169c58e8d2f809f220c6baa7469f26f9944f7c15
munetomo <fishmans0120@gmail.com>
2020-09-26 13:26:15 +0900

    update

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,30 @@ myImage.onclick = function() {
   }
 }
 
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('あなたの名前を入力してください');
+  if (!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozillaはすばらしいよ、' + myName;
+  }
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozillaはすばらしいよ、' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
 // const myHeading = document.querySelector('h1');
 // myHeading.textContent = 'Hello, world!';
 
